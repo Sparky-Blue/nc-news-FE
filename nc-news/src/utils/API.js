7846fb3629907cef14bb6c01a6e38bdde0c69464
Clare Pickering <clarelpickering@hotmail.com>
2018-04-06 19:52:15 +0100

    add vote functionality to comments

--- nc-news/src/utils/API.js
+++ nc-news/src/utils/API.js
@@ -37,7 +37,7 @@ const API = {
   },
   putCommentVote: (commentId, vote) => {
     return axios
-      .put(`${APIroute}/articles/${commentId}?vote=${vote}`)
+      .put(`${APIroute}/comments/${commentId}?vote=${vote}`)
       .then(response => response.data);
   }
 };
