077420b0e2ae90af45a21f66d7cb1ca81cdc0117
Clare Pickering <clarelpickering@hotmail.com>
2018-04-07 09:28:40 +0100

    add getDerviedStateFromProps and componentDidUpdate to update page for new topic

--- nc-news/src/components/ArticlesByTopic.js
+++ nc-news/src/components/ArticlesByTopic.js
@@ -5,15 +5,32 @@ import ArticlesFeed from "./ArticlesFeed";
 import sortArticlesByVotes from "../utils/sortArticlesByVotes";
 
 class ArticlesByTopic extends Component {
-  state = { articles: [] };
+  state = { articles: [], topic: null };
 
   componentDidMount() {
+    this.updateArticlesByTopic();
+  }
+
+  static getDerivedStateFromProps(newProps, prevState) {
+    if (newProps.match.params.topic !== prevState.topic) {
+      return {
+        topic: newProps.match.params.topic
+      };
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.topic !== this.props.match.params.topic) {
+      this.updateArticlesByTopic();
+    }
+  }
+
+  updateArticlesByTopic() {
     const topic = this.props.match.params.topic;
     API.getArticlesByTopic(topic).then(({ articles }) => {
       articles = sortArticlesByVotes(articles);
-      this.setState({
-        articles
-      });
+      this.setState({ articles, topic });
     });
   }
 
