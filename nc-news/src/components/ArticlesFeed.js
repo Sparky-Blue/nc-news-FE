76323d0131fc4047fe192c25cc5bf5ab7357402b
Clare Pickering <clarelpickering@hotmail.com>
2018-04-08 15:56:32 +0100

    refactor to stateless function

--- nc-news/src/components/ArticlesFeed.js
+++ nc-news/src/components/ArticlesFeed.js
@@ -1,22 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Loading from "./Loading";
-import PT from "prop-types";
 import ArticleDisplay from "./ArticleDisplay";
 
-class ArticleFeed extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.loading && <Loading loading={this.props.loading} />}
-        <ArticleDisplay articles={this.props.articles} />
-      </div>
-    );
-  }
-
-  static propTypes = {
-    articles: PT.array.isRequired,
-    loading: PT.bool
-  };
-}
+const ArticleFeed = ({ loading, articles }) => {
+  return (
+    <div>
+      {loading && <Loading loading={loading} />}
+      <ArticleDisplay articles={articles} />
+    </div>
+  );
+};
 
 export default ArticleFeed;
