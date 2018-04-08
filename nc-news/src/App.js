de1115b4ca6c6485ec3b599d00e1d495a32fd760
Clare Pickering <clarelpickering@hotmail.com>
2018-04-08 15:02:35 +0100

    refactor app to move data fetch onto home

--- nc-news/src/App.js
+++ nc-news/src/App.js
@@ -9,53 +9,14 @@ import Article from "./Screens/Article";
 import Footer from "./components/Footer";
 import API from "./utils/API";
 import sortBy from "./utils/sortBy";
-import _ from "underscore";
 
 class App extends Component {
-  state = {
-    articles: [],
-    loadingData: true,
-    username: null
-  };
-
-  componentDidMount() {
-    API.getArticles().then(({ articles }) => {
-      const articlesList = sortBy(articles, "votes");
-      this.setState({ articles: articlesList, loadingData: false });
-    });
-  }
-
-  getArticleById = id => {
-    let article = {};
-    if (this.state.articles.length === 0) return article;
-    article = _.findWhere(this.state.articles, { _id: id });
-    return article;
-  };
-
   render() {
     return (
       <Router>
         <div className="wrapper app">
-          <Header username={this.state.username} />
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <Home
-                {...this.props}
-                articles={this.state.articles}
-                loading={this.state.loadingData}
-              />
-            )}
-          />
-          <Route
-            path="/articles/:article_id"
-            render={props => {
-              return (
-                <Article {...props} getArticleById={this.getArticleById} />
-              );
-            }}
-          />
+          <Header />
+          <Route path="/" component={Home} />
           <Route path="/topics/:topic/articles" component={ArticlesByTopic} />
           <Route path="/users/:username" component={User} />
           <Footer />
