82153780913ed742cea632dfa2328f0dd30ca985
Clare Pickering <clarelpickering@hotmail.com>
2018-04-08 15:59:12 +0100

    remove unused parameter

--- nc-news/src/components/Header.js
+++ nc-news/src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 import TopicsMenu from "./TopicsMenu";
 
-const Header = ({ username }) => {
+const Header = () => {
   return (
     <header>
       <Link to="/">
