446cfe308c78a424b73956487b6a43a5670296d9
Clare Pickering <clarelpickering@hotmail.com>
2018-04-09 11:19:52 +0100

    add save on enter

--- nc-news/src/components/AddComment.js
+++ nc-news/src/components/AddComment.js
@@ -4,7 +4,14 @@ const AddComment = ({ postComment, newComment, eventHandler }) => {
   return (
     <div className="input" id="addComment">
       Add comment:
-      <input type="text" onChange={eventHandler} value={newComment} />
+      <input
+        type="text"
+        onChange={eventHandler}
+        value={newComment}
+        onKeyUp={e => {
+          if (e.key === "Enter") postComment(newComment);
+        }}
+      />
       <button onClick={() => postComment(newComment)}>Submit</button>
     </div>
   );
