dc441b8e2a69d5c8ad5c144299c9d37bb2c0f32e
Clare Pickering <clarelpickering@hotmail.com>
2018-04-09 09:57:39 +0100

    fix props issue with article vote on ArticleView

--- nc-news/src/components/ArticleView.js
+++ nc-news/src/components/ArticleView.js
@@ -10,27 +10,18 @@ const ArticleView = ({
   username,
   deleteComment
 }) => {
-  const {
-    body,
-    comments,
-    topic,
-    created_by,
-    article_id,
-    title,
-    votes
-  } = article;
+  const { body, comments, topic, created_by, _id, title, votes } = article;
   return (
     <div className="article">
       <h4>{title}</h4>
       <h5>Topic: {topic}</h5>
-      <Voter articleId={article_id} votes={votes} />
+      <Voter articleId={_id} votes={votes} />
       <h6>Comments: {comments}</h6>
       <h5>
         Author: <Link to={`/users/${created_by}`}>{created_by}</Link>
       </h5>
       <p className="body">{body}</p>
       <Comments
-        articleId={article_id}
         comments={allComments}
         postComment={postComment}
         username={username}
