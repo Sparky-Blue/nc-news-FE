55ba6f2f269d38f92e3bfbaee386368c6c09dfdc
Clare Pickering <clarelpickering@hotmail.com>
2018-04-09 08:00:45 +0100

    add search functionality

--- nc-news/src/components/ArticlesFeed.js
+++ nc-news/src/components/ArticlesFeed.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Loading from "./Loading";
 import ArticleDisplay from "./ArticleDisplay";
+import Search from "./Search";
 
 const ArticleFeed = ({ loading, articles }) => {
   return (
     <div>
       {loading && <Loading loading={loading} />}
+      <Search articles={articles} />
       <ArticleDisplay articles={articles} />
     </div>
   );
--- nc-news/src/components/Search.js
+++ nc-news/src/components/Search.js
@@ -1,14 +1,73 @@
 import React, { Component } from "react";
+import SearchInput, { createFilter } from "react-search-input";
+import ArticleDisplay from "./ArticleDisplay";
 
 class Search extends Component {
+  KEYS_TO_FILTERS = ["title", "body", "created_by"];
+  state = {
+    searchTerm: "",
+    filteredArticles: [],
+    serchActive: false
+  };
+
+  searchUpdated = term => {
+    let searchActive = false;
+    if (this.state.searchTerm.length > 0) searchActive = true;
+    this.setState({ searchTerm: term, searchActive });
+    this.filterArticles();
+  };
+
+  filterArticles = () => {
+    const filteredArticles = this.props.articles.filter(
+      createFilter(this.state.searchTerm, this.KEYS_TO_FILTERS)
+    );
+    this.setState({
+      filteredArticles
+    });
+  };
+
   render() {
     return (
-      <label>
-        <i className="fas fa-search" />
-        <input type="text" name="Search" id="search" />
-      </label>
+      // <label>
+      //   <i className="fas fa-search" />
+      //   <input type="text" name="Search" id="search" />
+      // </label>
+      <div>
+        <SearchInput className="search-input" onChange={this.searchUpdated} />
+        <ArticleDisplay articles={this.state.filteredArticles} />
+      </div>
     );
   }
 }
 
 export default Search;
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       searchTerm: ""
+//     };
+//     this.searchUpdated = this.searchUpdated.bind(this);
+//   }
+
+//   render() {
+//     const fi = emails.filter(
+//       createFilter(this.state.searchTerm, KEYS_TO_FILTERS)
+//     );
+
+//     return (
+//       <div>
+//         <SearchInput className="search-input" onChange={this.searchUpdated} />
+//         {filteredEmails.map(email => {
+//           return (
+//             <div className="mail" key={email.id}>
+//               <div className="from">{email.user.name}</div>
+//               <div className="subject">{email.subject}</div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
