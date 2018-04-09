1dd567769f713a3719c6c47968c71f51b3c8b976
Clare Pickering <clarelpickering@hotmail.com>
2018-04-09 09:00:46 +0100

    style search feed

--- nc-news/src/components/ArticlesFeed.js
+++ nc-news/src/components/ArticlesFeed.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Loading from "./Loading";
 import ArticleDisplay from "./ArticleDisplay";
 import Search from "./Search";
 
-const ArticleFeed = ({ loading, articles }) => {
-  return (
-    <div>
-      {loading && <Loading loading={loading} />}
-      <Search articles={articles} />
-      <ArticleDisplay articles={articles} />
-    </div>
-  );
-};
+class ArticleFeed extends Component {
+  state = {
+    searchActive: false
+  };
+
+  changeSearchStatus = searchTerm => {
+    let searchActive = false;
+    if (searchTerm.length > 0) searchActive = true;
+  };
+
+  render() {
+    const { loading, articles } = this.props;
+    return (
+      <div>
+        {loading && <Loading loading={loading} />}
+        <Search
+          articles={articles}
+          changeSearchStatus={this.changeSearchStatus}
+        />
+        {!this.state.searchActive && <ArticleDisplay articles={articles} />}
+      </div>
+    );
+  }
+}
 
 export default ArticleFeed;
