40b3d2916c7e7729c0a6870b30ceb5412c45d25f
Clare Pickering <clarelpickering@hotmail.com>
2018-04-09 09:28:42 +0100

    tidy up search component

--- nc-news/src/components/Search.js
+++ nc-news/src/components/Search.js
@@ -26,10 +26,6 @@ class Search extends Component {
 
   render() {
     return (
-      // <label>
-      //   <i className="fas fa-search" />
-      //   <input type="text" name="Search" id="search" />
-      // </label>
       <div>
         <SearchInput className="search-input" onChange={this.searchUpdated} />
         <ArticleDisplay articles={this.state.filteredArticles} />
@@ -39,33 +35,3 @@ class Search extends Component {
 }
 
 export default Search;
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       searchTerm: ""
-//     };
-//     this.searchUpdated = this.searchUpdated.bind(this);
-//   }
-
-//   render() {
-//     const fi = emails.filter(
-//       createFilter(this.state.searchTerm, KEYS_TO_FILTERS)
-//     );
-
-//     return (
-//       <div>
-//         <SearchInput className="search-input" onChange={this.searchUpdated} />
-//         {filteredEmails.map(email => {
-//           return (
-//             <div className="mail" key={email.id}>
-//               <div className="from">{email.user.name}</div>
-//               <div className="subject">{email.subject}</div>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     );
-//   }
-// }
